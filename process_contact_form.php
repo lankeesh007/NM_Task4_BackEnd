40482568c40b02339f889ea7f2df7ff01f1c14bd
lankeesh007 <149468443+lankeesh007@users.noreply.github.com>
2023-11-08 15:41:34 +0530

    Add files via upload

---
+++ process_contact_form.php
@@ -0,0 +1,37 @@
+<?php
+// Database connection parameters
+$db_host = "localhost";
+$db_user = "DB";
+$db_pass = "jaganjn@123";
+$db_name = "naga";
+
+// Connect to the database
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Insert data into the database
+    $sql = "INSERT INTO contact_form (name, email, message) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $name, $email, $message);
+
+    if ($stmt->execute()) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+}
+
+// Close the database connection
+$conn->close();
+?>
